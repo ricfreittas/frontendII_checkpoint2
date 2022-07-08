e807a9863cab11630b7904638c8828f471e27cd3
Ricardo Freitas <ric.freitas@msn.com>
2022-07-07 21:31:52 -0300

    Add files via upload
    Atualização login.html / signup.html / login.js / signup.html

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,29 +1,22 @@
-const formControlsElements = document.querySelectorAll('.form-control')
 const createUserButtonElement = document.querySelector('#createUserButton')
+const formControlsElements = document.querySelectorAll('.form-control')
+// Obtenção de Todos os Inputs do meu HTML em formato de Array
+const allInputsElements = document.querySelectorAll('input')
 
+//confereSenha
+//const firstKey = document.querySelector('#password')
+//const confirKey = document.querySelector('#passwordConfirm')
+
+// código linha 7 até 38 se refere a validação dos campos
 var formValidation = {
-    userName: false,
-    userSobrenome: false,
-    userEmail: false,
-    userPassword: false,
-    userPasswordConfirm: false
+    firstName: false,
+    lastName: false,
+    email: false,
+    password: false,
+    passwordConfirm: false
 }
 
-createUserButtonElement.addEventListener( 'click', event => {
-
-    event.preventDefault()
-
-    let formValid = Object.values(formValidation).every(Boolean)
-
-    if(formValid) {
-
-        window.location = './pages/login.html'
-
-    }
-
-})
-
-for(let control of formControlsElements) {
+for (let control of formControlsElements) {
 
     const controlInputElement = control.children[1]
 
@@ -33,7 +26,8 @@ for(let control of formControlsElements) {
 
         formValidation[event.target.id] = inputValid
 
-        if(inputValid) {
+
+        if (inputValid) {
 
             control.classList.remove('error')
 
@@ -47,6 +41,104 @@ for(let control of formControlsElements) {
 
 }
 
-var name1 = 'Ricardo'
+//function confereSenha(){
 
-console.log(name1)
+  //  if(confirKey.value === firstKey.value){
+  //      confirKey.setCustomValidity('');
+   // } else {
+    //    confirKey.setCustomValidity('As senhas digitadas não conferem');
+  //  }
+
+//}
+
+////password.onchange = validatePassword;
+//confirm_password.onkeyup = validatePassword;
+
+for(let input of allInputsElements) {
+
+    input.addEventListener('keyup', event => {
+
+        const inputValue = input.value
+        const inputId = input.id
+
+        formData[inputId] = inputValue
+
+        console.log(formData)
+
+    })
+
+}
+
+var formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+}
+
+var requestHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+// Variavel que irá conter o nosso objeto de configuração da requisição
+var requestPostConfiguration = {
+    method: 'POST',
+    headers: requestHeaders
+}
+
+function createUser() {
+
+    requestPostConfiguration.body = JSON.stringify(formData)
+
+// O Fetch é responsável por fazer uma requisição para um back-end
+      // O parametro do fetch serve justamente para especificarmos aonde ele irá fazer a requisição
+      fetch('https://ctd-todo-api.herokuapp.com/v1/users', requestPostConfiguration).then(
+
+        response => {
+
+            response.json().then(
+
+                info => {
+
+                    if(response.ok == true) {
+
+                        alert('Parabens! Usuário criado com sucesso.')
+
+                    } else {
+
+                        if(info === 'El usuario ya se encuentra registrado') {
+
+                            alert('O e-mail digitado ja esta cadastrado')
+
+                        }
+
+                    }
+
+                }
+
+            )
+
+        }
+
+    )
+
+}
+
+createUserButtonElement.addEventListener('click', event => {
+
+    event.preventDefault()
+
+    let formValid = Object.values(formValidation).every(Boolean)
+
+    if (formValid) {
+
+        createUser()
+        
+
+    } else {
+
+        alert('crash')
+    }
+})
+
